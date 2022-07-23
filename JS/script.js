76c9befaf4426aefc85f9777a839e82a4ebdc6e1
CollinsNjane <njanecollins@gmail.com>
2022-07-23 12:39:33 +0200

    Added some comments

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
+// Calculator class that can be used to create calculator object
 class Calculator {
+  //Constructor that initializes variables
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
@@ -6,20 +8,24 @@ class Calculator {
     this.previousOperand = "";
     this.operation = undefined;
   }
+  //Method to clear the calculator display
   clear() {
     this.currentOperand = "";
     this.previousOperand = "";
     this.operation = undefined;
   }
+  //Method to delete a digit from the screen
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
+  //Method to add number before operation is done
   appendNumber(number) {
     if (number === "." && this.currentOperand.includes(".")) {
       return;
     }
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
+  //Method that chooses which operation, it assigns a value to previous operand
   chooseOperation(operation) {
     //Not more than 1 pair of numbers
     if (this.currentOperand === "") {
@@ -32,41 +38,19 @@ class Calculator {
     this.previousOperand = this.currentOperand;
     this.currentOperand = "";
   }
-  compute() {
-    let computation;
-    const prev = parseFloat(this.previousOperand);
-    const current = parseFloat(this.currentOperand);
-    if (isNaN(prev) || isNaN(current)) {
-      return;
-    }
-    computation = this.noOverflow(
-      this.operate(this.operation, prev, current),
-      10
-    );
-
-    //console.log(computation);
-    this.currentOperand = computation;
-    this.operation = undefined;
-    this.previousOperand = "";
-  }
-  updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    //display operator on previous operand
-    if (this.operation != null) {
-      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
-    } else {
-      this.previousOperandTextElement.innerText = "";
-    }
-  }
+  //Method to add numbers
   add(number1, number2) {
     return number1 + number2;
   }
+  //Method to subtract numbers
   subtract(number1, number2) {
     return number1 - number2;
   }
+  //Method to multiply numbers
   multiply(number1, number2) {
     return number1 * number2;
   }
+  //Method to divide numbers
   divide(number1, number2) {
     if (number2 === 0) {
       return "Error";
@@ -74,9 +58,11 @@ class Calculator {
       return number1 / number2;
     }
   }
+  //Method Rounds number to nearest places, default is 10 decimaal places
   noOverflow(num, places) {
     return parseFloat(Math.round(num + "e" + places) + "e-" + places);
   }
+  //Operate function that takes in an operator and 2 numbers and calculates the value accordingly
   operate(operator, number1, number2) {
     let num1 = Number(number1);
     let num2 = Number(number2);
@@ -93,7 +79,38 @@ class Calculator {
         return;
     }
   }
+  //Method to compute the whole operation
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    //If the current and previous numbers are not numbers, break out of the compute method
+    if (isNaN(prev) || isNaN(current)) {
+      return;
+    }
+    computation = this.noOverflow(
+      this.operate(this.operation, prev, current),
+      10
+    );
+
+    //console.log(computation);
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+  //Update the display that is called after buttons are clicked
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+    //display operator on previous operand
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
+    }
+  }
+  
 }
+//DOM variables
 const numberKeys = document.querySelectorAll("[data-number]");
 const operationKeys = document.querySelectorAll("[data-operator]");
 const equalButton = document.querySelector("[data-equals]");
@@ -105,11 +122,12 @@ const previousOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
-
+//A new calculator object
 const calculator = new Calculator(
   previousOperandTextElement,
   currentOperandTextElement
 );
+//Event Listeners
 numberKeys.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText);
